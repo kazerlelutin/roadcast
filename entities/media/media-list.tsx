32c157b65b8ad10fde95c92b801a665a1dc54df2
Kazerlelutin <b@bouteiller.contact>
2023-04-27 14:30:23 +0200

    media opacity for focus mode

--- entities/media/media-list.tsx
+++ entities/media/media-list.tsx
@@ -1,15 +1,24 @@
 import { useContext } from 'react'
-import { ChronicleContext } from '../chronicle/chronicle'
+import {
+  ChronicleContext,
+  ChronicleToScreenContext,
+} from '../chronicle/chronicle'
 import { MediaProvider } from './media'
 import { MediaWithControls } from './media-with-controls'
 import styles from './media-styles/media-list.module.css'
+import { BroadcastFocusContext } from '../broadcast/broadcast'
 
 export const MediaList: React.FC = () => {
+  const [currentChronicle] = useContext(ChronicleToScreenContext)
+  const [focusMode] = useContext(BroadcastFocusContext)
   const [chronicle] = useContext(ChronicleContext)
 
   if (chronicle.medias.length === 0) return <></>
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      data-visible={!focusMode || currentChronicle === chronicle.id}
+    >
       {chronicle.medias.map((media) => (
         <MediaProvider key={media.id} media={media}>
           <MediaWithControls />
