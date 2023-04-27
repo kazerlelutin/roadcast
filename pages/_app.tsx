f631825bdbb28cdf3f4520ef6550d8ae83834b27
Kazerlelutin <b@bouteiller.contact>
2023-04-27 21:39:37 +0200

    update head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,9 @@ import { DndProvider } from 'react-dnd'
 import NProgress from 'nprogress'
 import { Router } from 'next/router'
 import { FullscreenPopinProvider } from '../ui/fullscreen-popin/fullscreen-popin.provider'
+import { useTranslate } from '../hooks/translate.hook'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 NProgress.configure({ showSpinner: false, easing: 'ease', speed: 500 })
 Router.events.on('routeChangeStart', () => NProgress.start())
@@ -13,10 +16,32 @@ Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  const baseUrl = 'https://roadcast.fr/'
+  const t = useTranslate({
+    title: {
+      fr: '📡 ROADCAST',
+      en: '📡 ROADCAST',
+    },
+  })
   return (
     <DndProvider backend={HTML5Backend}>
       <MiniLoaderProvider>
         <FullscreenPopinProvider>
+          <Head>
+            <title>{t('title')}</title>
+            <link rel="icon" href="/favicon.ico" />
+            <meta name="description" content={t('appSummary')} />
+            <meta property="og:title" content={t('title')} />
+            <meta property="og:type" content="siteweb" />
+            <meta property="og:image" content={`${baseUrl}preview.webp`} />
+            <meta name="twitter:card" content="photo" />
+            <meta name="twitter:site" content={baseUrl} />
+            <meta name="twitter:title" content={t('title')} />
+            <meta name="twitter:description" content={t('appSummary')} />
+            <meta name="twitter:image" content={`${baseUrl}preview.webp`} />
+            <meta name="twitter:url" content={`${baseUrl}${router.asPath}`} />
+          </Head>
           <div id="fullscreen-popin" />
           <Component {...pageProps} />
         </FullscreenPopinProvider>
