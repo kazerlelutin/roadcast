d1a63621d7fbe61c5a0ab7b82d3dc7ac57d9783a
Kazerlelutin <b@bouteiller.contact>
2023-05-27 13:15:16 +0200

    fix import

--- ui/roadcast-logo/roadcast-logo.tsx
+++ ui/roadcast-logo/roadcast-logo.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link'
 import { FC } from 'react'
 import styles from './roadcast-logo.module.css'
-import { version } from '../../package.json'
+import * as pkg from '../../package.json'
+const { version } = pkg
 
 export const RoadcastLogo: FC = () => (
   <div className={styles.logo} title={version}>
