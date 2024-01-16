4cd2c207507a76a595b46620ce7e5e35381a15a5
Kazerlelutin <b@bouteiller.contact>
2024-01-16 21:00:51 +0100

    wip

--- front/ctrl/chronicleTree.js
+++ front/ctrl/chronicleTree.js
@@ -1,9 +1,24 @@
+import { getLsLock } from './lock'
+
 export const chronicleTree = {
+  onInit(_state, el) {
+    const ls = getLsLock()
+    if (ls === 'lock') {
+      const grabIcon = el.querySelector('[data-grab-icon')
+
+      if (grabIcon) grabIcon.classList.add('hidden')
+    }
+  },
   onDragstart(state, _el, e) {
     e.dataTransfer.setData('text/plain', state.chronicle_id)
   },
-  render(state, _el, listen) {
-    if (listen) {
+  render(state, el, listen) {
+    if (listen.key === 'lock') {
+      const grabIcon = el.querySelector('[data-grab-icon')
+      if (grabIcon)
+        grabIcon.classList[listen.value ? 'add' : 'remove']('hidden')
+    }
+    if (listen.key === 'chronicle') {
       const titleEl = document.querySelector(
         `[data-title="${state.chronicle_id}"]`
       )
--- front/ctrl/tree.js
+++ front/ctrl/tree.js
@@ -11,20 +11,21 @@ export const tree = {
     const { chronicles } = broadcastEl.state.broadcast
 
     container.innerHTML = ''
-    for (const chronicle of chronicles) {
+    for (const [index, chronicle] of chronicles.entries()) {
       const chronicleTreeEl = await kll.processTemplate('chronicleTree')
 
       chronicleTreeEl.setAttribute('id', `tree_${chronicle.id}`)
       chronicleTreeEl.setAttribute('kll-b', `${chronicle.id}.chronicle`)
 
+      const addPost = index === chronicles.length - 1 ? 0 : 1
       setElementbyKllTc(chronicleTreeEl, 'chronicleTreeDropZone', null, {
         'kll-id': `dropzone_${chronicle.position + 1}`,
-        'kll-s-position': chronicle.position + 1,
+        'kll-s-position': chronicle.position + addPost,
         id: `dropzone_${chronicle.id}`
       })
 
       setElement(chronicleTreeEl, 'grab', undefined, {
-        'kll-b': `${chronicle.id}.chronicle`,
+        'kll-b': `${chronicle.id}.chronicle,lock.lock`,
         'kll-id': `chronicle_tree_title_${chronicle.id}`,
         'kll-s-position': chronicle.position,
         'kll-s-chronicle_id': chronicle.id
