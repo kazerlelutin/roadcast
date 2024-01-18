365b96ec43daff5676cc42c00942dc0fdff5e99e
Kazerlelutin <b@bouteiller.contact>
2024-01-18 23:35:03 +0100

    wip

--- front/ctrl/addMedia.js
+++ front/ctrl/addMedia.js
@@ -1,11 +1,17 @@
 export const addMedia = {
-    state: {
-    },
-    onInit(state, el, e) {
+  state: {},
+  onInit(state, el, e) {
+    console.log('oninit', el)
+  },
+  async onchange(state, el, e) {
+    console.log('onsubmit', e.target.files)
 
+    //TODO ici envoyer les fichiers au serveur
+    //TODO faire les dropzone
+    /*
 
-    },
-    render() {
-
-    },
-  };
+    peut être qu'il faudrait faire descendre l'input pour faire la dropzone
+    */
+  },
+  render() {}
+}
--- front/ctrl/medias.js
+++ front/ctrl/medias.js
@@ -1,15 +1,42 @@
+import { kll } from '../main'
 import { getState } from '../utils/getState'
+import { setAttributes } from '../utils/setElement'
 
 export const medias = {
   state: {
     chronicle_id: null
   },
-  onInit(state, el, e) {
+  onInit(_state, el) {
+    el.render()
+  },
+  async render(state, el, listen) {
     const { chronicle } = getState(state.chronicle_id)
 
-    console.log('ici on init les medias', chronicle)
-  },
-  render(state, el, listen) {
-    console.log('ici on génère les medias')
+    if (!chronicle) return
+    el.innerHTML = ''
+
+    for (const media of chronicle.medias) {
+      const template = await kll.processTemplate('media')
+
+      setAttributes(template, {
+        'kll-id': media.id,
+        'kll-s-id': media.id
+      })
+
+      el.appendChild(template)
+    }
+
+    const addMedias = await kll.processTemplate('addMedia')
+
+    setAttributes(addMedias, {
+      'kll-id': `add_media_${chronicle.id}`,
+      'kll-ctrl': 'addMedia',
+      id: `add_media_${chronicle.id}`,
+      'kll-s-chronicle_id': chronicle.id
+    })
+
+    el.appendChild(addMedias)
+
+    kll.reload(el)
   }
 }
